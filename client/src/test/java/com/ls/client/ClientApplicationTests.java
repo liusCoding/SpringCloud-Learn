e504d84e5b8d4b78641ad44b84f5591318ebe714
liusCoding <14786469221@163.com>
2020-04-02 18:48:21 +0800

    初始化eureka Client项目

---
+++ client/src/test/java/com/ls/client/ClientApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ls.client;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
